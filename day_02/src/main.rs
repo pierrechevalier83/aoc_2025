cfc2a312e8dcbb5371d04b54be650820058da7ad
Pierre Chevalier <pierrechevalier83@gmail.com>
2025-12-02 07:08:09 +0000

    Day 02, Part 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,13 +1,39 @@
 #![feature(trim_prefix_suffix)]
 
-fn is_invalid_id(id: &usize) -> bool {
-    let num_digits = (*id).ilog10() + 1;
-    num_digits % 2 == 0
-        && id / 10_usize.pow(num_digits / 2)
-            == id - id / 10_usize.pow(num_digits / 2) * 10_usize.pow(num_digits / 2)
+#[derive(Clone, Copy)]
+enum ValidityProtocol {
+    Part1,
+    Part2,
 }
 
-fn sum_invalid_ids(input: &str) -> usize {
+fn repeats(id: usize, num_digits: u32, chunks: u32) -> bool {
+    if num_digits == chunks {
+        true
+    } else {
+        let first_digits = id / 10_usize.pow(num_digits - chunks);
+        let last_digits = id - id / 10_usize.pow(chunks) * 10_usize.pow(chunks);
+        let all_but_first_digits = id - first_digits * 10_usize.pow(num_digits - chunks);
+        first_digits == last_digits && repeats(all_but_first_digits, num_digits - chunks, chunks)
+    }
+}
+
+fn is_invalid_id(id: usize, protocol: ValidityProtocol) -> bool {
+    let num_digits = id.ilog10() + 1;
+    match protocol {
+        ValidityProtocol::Part1 => {
+            if num_digits % 2 == 0 {
+                repeats(id, num_digits, num_digits / 2)
+            } else {
+                false
+            }
+        }
+        ValidityProtocol::Part2 => {
+            (1..=(num_digits / 2)).any(|chunks| repeats(id, num_digits, chunks))
+        }
+    }
+}
+
+fn sum_invalid_ids(input: &str, protocol: ValidityProtocol) -> usize {
     input
         .trim_suffix("\n")
         .split(",")
@@ -15,13 +41,20 @@ fn sum_invalid_ids(input: &str) -> usize {
             let (start, end) = range.split_once("-").unwrap();
             start.parse().unwrap()..=end.parse().unwrap()
         })
-        .filter(is_invalid_id)
+        .filter(|id| is_invalid_id(*id, protocol))
         .sum()
 }
 
 fn main() {
     let input = include_str!("../input.txt");
-    println!("Part 1: {}", sum_invalid_ids(input));
+    println!(
+        "Part 1: {}",
+        sum_invalid_ids(input, ValidityProtocol::Part1)
+    );
+    println!(
+        "Part 2: {}",
+        sum_invalid_ids(input, ValidityProtocol::Part2)
+    );
 }
 
 #[cfg(test)]
@@ -30,7 +63,22 @@ mod tests {
 
     use super::*;
     #[test]
+    fn test_repeats() {
+        assert!(repeats(141414, 6, 2));
+        assert!(!repeats(141141, 6, 2));
+        assert!(repeats(141141, 6, 3));
+        assert!(!repeats(101, 3, 1));
+    }
+
+    #[test]
     fn test_sum_invalid_ids() {
-        assert_eq!(1227775554, sum_invalid_ids(TEST_INPUT));
+        assert_eq!(
+            1227775554,
+            sum_invalid_ids(TEST_INPUT, ValidityProtocol::Part1)
+        );
+        assert_eq!(
+            4174379265,
+            sum_invalid_ids(TEST_INPUT, ValidityProtocol::Part2)
+        );
     }
 }
