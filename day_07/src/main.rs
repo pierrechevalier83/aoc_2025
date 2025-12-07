f6b6a8dec2ff427c2efa5df0f5da2c58b31a009c
Pierre Chevalier <pierrechevalier83@gmail.com>
2025-12-07 05:48:29 +0000

    Day 07, Part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -47,7 +47,35 @@ fn count_splits(input: &[u8]) -> usize {
         .sum()
 }
 
+fn count_timelines(input: &[u8]) -> usize {
+    let mut rays: Vec<usize> = input
+        .split_once(|b| *b == b'\n')
+        .unwrap()
+        .0
+        .iter()
+        .map(|b| if *b == b'S' { 1 } else { 0 })
+        .collect();
+    input.split(|b| *b == b'\n').skip(1).for_each(|line| {
+        let mut next_rays = rays.clone();
+        for (index, (b, ray)) in line.into_iter().zip(&rays).enumerate() {
+            if *b == b'^' && *ray > 0 {
+                next_rays[index] = 0;
+                next_rays[index + 1] += ray;
+                next_rays[index - 1] += ray;
+            }
+        }
+
+        rays = next_rays.clone();
+    });
+    rays.iter().sum()
+}
+
 fn main() {
     println!("Part 1 - test: {}", count_splits(TEST_INPUT));
     println!("Part 1: {}", count_splits(include_bytes!("../input.txt")));
+    println!("Part 2 - test: {}", count_timelines(TEST_INPUT));
+    println!(
+        "Part 2 - test: {}",
+        count_timelines(include_bytes!("../input.txt"))
+    );
 }
